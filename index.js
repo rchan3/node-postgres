2d8207a12e6788528e34fc33af489eedb5dd3665
rchan3 <r.achiechan@gmail.com>
2022-06-21 17:39:51 -0400

    working version of echo PoC

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const login = require("./login");
 const signup = require("./signup");
 const addscore = require("./addscore");
-const webhook = require("./webhook");
+
 const { response } = require("express");
 require("dotenv").config();
 
@@ -55,14 +55,34 @@ app.post("/addscore", (req, res) => {
 });
 
 app.post("/webhook", (req, res) => {
-  webhook
-    .echo(req.body.contact.displayName, req.body.message.content.text)
-    .then((response) => {
-      res.status(200).send(response);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
+  var messagebird = require("messagebird")(process.env.LIVE_API_KEY, 10000);
+
+  var params = {
+    to: req.body.contact.displayName,
+    from: process.env.WHATSAPP_CHANNEL_ID,
+    type: "text",
+    content: {
+      text: req.body.message.content.text,
+      disableUrlPreview: false,
+    },
+  };
+  console.log("sending message: " + req.body.message.content.text + " to");
+  console.log(req.body.contact.displayName);
+
+  //check so bot only replies to user sent messages
+
+  if (req.body.message.direction == "sent") {
+    res.send(console.log("message is from operator"));
+  } else {
+    res.send(console.log("message from user"));
+    messagebird.conversations.send(params, function (err, response) {
+      if (err) {
+        res.send(console.log(err));
+      }
+      console.log(response);
+      res.send(console.log(response));
     });
+  }
 });
 
 app.listen(port, () => {
